ef39a43fb1e78c39278ef260d7cc86c6801d4d9a
Ed <emmorlu@gmail.com>
2026-06-08 02:31:05 -0400

    feat: add vercel analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Analytics } from '@vercel/analytics/next';
+
 import MapComponent from './components/Map';
 
 export default function Home() {
     return (
         <main className="flex flex-1 w-full flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
             <MapComponent />
+            <Analytics />
         </main>
     );
 }
